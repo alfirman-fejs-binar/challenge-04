f8a996fc0e52b1875ec57cf53b96ca79955a5adc
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-04-02 18:50:11 +0700

    feat: add UI

---
+++ src/components/Hero/index.js
@@ -0,0 +1,36 @@
+import Search from "../Search";
+
+export default function Hero() {
+  return (
+    <section id="hero">
+      <div className="hero-container">
+        <div>
+          <div className="left">
+            <div>
+              <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+              <p>
+                Selamat datang di Binar Car Rental. Kami menyediakan mobil
+                kualitas terbaik dengan harga terjangkau. Selalu siap melayani
+                kebutuhanmu untuk sewa mobil selama 24 jam.
+              </p>
+              <button>Mulai Sewa Mobil</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="right">
+            <img
+              src="https://raw.githubusercontent.com/alfirmanpahlepi/binar-car-rental/main/assets/image/Mercedes%20Car%20EQC%20300kW%20Edition%20-%20900x598%201.png"
+              alt="Mercedes Car EQC 300kW Edition - 900x598 1.png"
+              width="100%"
+              height="100%"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <Search />
+      </div>
+    </section>
+  );
+}
